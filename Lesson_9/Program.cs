a142bb781e459af120750f0e5a924710980544be
Imil <Mil2163444@mail.ru>
2023-02-28 20:36:16 +0300

    Урок 9

---
+++ Lesson_9/Program.cs
@@ -0,0 +1,4 @@
+﻿//9. Показать последнюю цифру трёхзначного числа
+System.Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{number %10} Последняя цифра");

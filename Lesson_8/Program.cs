93ed6c2930460f0ef05393bcb1687bd6127c57f8
Imil <Mil2163444@mail.ru>
2023-02-28 19:14:30 +0300

    Урок 8

---
+++ Lesson_8/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Введите размер массива: ");
+int massa = Convert.ToInt32(Console.ReadLine());
+int[]array = new int [massa];
+int sum = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+System.Console.Write($"Введите элемент массива под индексом {i}: ");
+ array[i]= Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 1; i < array.Length; i += 2)
+  {
+    sum += array[i];
+  }
+System.Console.WriteLine($"Сумма чисел чётных индексов: {sum}");
+

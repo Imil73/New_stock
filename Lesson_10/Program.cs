846032160e6417f4d06d4929ca2c7ceecb746702
Imil <Mil2163444@mail.ru>
2023-02-28 22:19:24 +0300

    Урок 10

---
+++ Lesson_10/Program.cs
@@ -0,0 +1,10 @@
+﻿//7. Показать числа от -N до N
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[]array = new int[(number*2)+1];
+for(int i = number*2; i >= 0 ; i--)
+   {
+    array[i] = number;
+   number-=1;
+   System.Console.WriteLine(array[i]);
+   }

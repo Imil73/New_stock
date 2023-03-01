1d5b3c7362fe318b5ef8b84f24f13ddec894f3bc
Imil <Mil2163444@mail.ru>
2023-03-01 09:34:34 +0300

    Урок 40

---
+++ Lesson_40/Program.cs
@@ -0,0 +1,22 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+System.Console.WriteLine("Укажите размер массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+
+for(int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"Укажите вещественное число индексу {i}: ");
+    array[i] = Convert.ToInt32(System.Console.ReadLine());
+    System.Console.WriteLine(array[i]);
+}
+int max = array[0];
+int min =array[0];
+
+for(int i = 1; i < number; i++)
+{
+   if(array[i] > max) max = array[i];
+   if(array[i] < min) min = array[i];
+}
+System.Console.Write($"Разность Max и Min равна: {(max-min)}");
+

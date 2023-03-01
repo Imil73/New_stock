c94c6d54b1120f4182843f18b0651a206d6d0126
Imil <Mil2163444@mail.ru>
2023-03-01 18:43:13 +0300

    посчитайте сколько чисел больше нуля

---
+++ Free_lesson_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Пользователь вводит с клавиатуры М чисел, 
+//посчитайте сколько чисел больше нуля?
+//( 0, 7, 8, -2, -2 )
+System.Console.Write("Какое количество чисел будет проверяться: ");
+ int number= int.Parse(Console.ReadLine());
+int[] array = new int[number];
+
+for (int i = 0; i < array.Length; i++)
+{
+System.Console.WriteLine($"Заполните ячейки числами {i}: ");
+array[i]= int.Parse(System.Console.ReadLine()); 
+}
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) sum += 1;
+    }
+System.Console.WriteLine($"В массиве {sum} числа больше нуля.");

275f1d4e6f4ac6002b3384089735005ae11b2e3b
Imil <Mil2163444@mail.ru>
2023-03-01 16:18:55 +0300

    Показать четные числа от 1 до N

---
+++ Lesson_08/Program.cs
@@ -0,0 +1,11 @@
+﻿//8. Показать четные числа от 1 до N
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int[] array = new int[number];
+int cifr1 = 1;
+for(int i = 0; i < array.Length; i++ )
+{
+    array [i] = cifr1;
+    System.Console.Write(array[i]); 
+    cifr1 += 1;
+}

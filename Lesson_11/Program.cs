4e3e6ac29cd5f7186865f342211ce519748fe65c
Imil <Mil2163444@mail.ru>
2023-03-02 11:32:14 +0300

    11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ Lesson_11/Program.cs
@@ -0,0 +1,13 @@
+﻿//11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+System.Console.WriteLine("Задайте число: ");
+int number = int.Parse(Console.ReadLine());
+int numberA = number / 10;
+int numberB = number %10;
+if(numberA < numberB)
+{
+    System.Console.WriteLine($"{numberB}");
+}
+else
+{
+   System.Console.WriteLine($"{numberA}"); 
+}

575f5f1fc9c15ebf4f05e1ffc4a5affa7ba27ab8
Imil <Mil2163444@mail.ru>
2023-03-02 12:05:11 +0300

    12. Удалить вторую цифру трёхзначного числа

---
+++ Lesson_12/Program.cs
@@ -0,0 +1,9 @@
+﻿//12. Удалить вторую цифру трёхзначного числа
+System.Console.WriteLine("Задайте число: ");
+int number = int.Parse(Console.ReadLine());
+
+int numberA = number / 100;
+int numberB = number %10;
+int res = numberA * 10 + numberB;
+
+System.Console.WriteLine($"{res}");

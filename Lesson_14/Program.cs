fbe219d75ace11e89dd6a350587aed7473dd6f48
Imil <Mil2163444@mail.ru>
2023-03-02 14:38:58 +0300

    14. Найти третью цифру числа или сообщить, что её нет

---
+++ Lesson_14/Program.cs
@@ -0,0 +1,14 @@
+﻿//14. Найти третью цифру числа или сообщить, что её нет
+System.Console.WriteLine("Задайте число для проверки: ");
+int cifr = int.Parse(Console.ReadLine());
+
+if (cifr / 100 == 0)
+{
+      System.Console.WriteLine($"У числа {cifr} нет третьей цифры.");
+}
+else
+{
+    while ( cifr > 1000 ) cifr = cifr /10;      
+      int res = cifr % 10;
+      System.Console.WriteLine($"Третья цифра числа {res}");
+}

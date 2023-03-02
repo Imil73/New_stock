0b9853eb6bc1c4bbfdd126febd8ece07436830d1
Imil <Mil2163444@mail.ru>
2023-03-02 12:58:56 +0300

    Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ Lesson_13/Program.cs
@@ -0,0 +1,15 @@
+﻿//13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+System.Console.WriteLine("Задайте число: ");
+int numberA = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Задайте число для проверки: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int res = numberA % numberB;
+if (res == 0)
+{
+    System.Console.WriteLine($"Число {numberA} кратно {numberB}");
+}
+else
+{
+    System.Console.WriteLine($"{res}");
+}
